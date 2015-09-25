512c8530c4774aeddaed30f5550b23f8d8f1e6b0
Ales Justin <ales.justin@gmail.com>
2015-09-25 15:05:33 +0200

    Add multiple deploments test.

---
+++ tests/src/test/java/org/jboss/test/ce/testsuite/common/MultipleDeploymentsTest.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.ce.testsuite.common;
+
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class MultipleDeploymentsTest {
+    private static Logger log = Logger.getLogger(MultipleDeploymentsTest.class.getName());
+
+    @Deployment(name = "A")
+    public static WebArchive getDeploymentA() throws Exception {
+        return ShrinkWrap.create(WebArchive.class, "testA.war");
+    }
+
+    @Deployment(name = "B")
+    public static WebArchive getDeploymentB() throws Exception {
+        return ShrinkWrap.create(WebArchive.class, "testB.war");
+    }
+
+    @Test
+    @OperateOnDeployment("A")
+    public void testPokeA() throws Exception {
+        log.info("A!");
+    }
+
+    @Test
+    @OperateOnDeployment("B")
+    public void testPokeB() throws Exception {
+        log.info("B!");
+    }
+}
