1187e03cb10be7151c7f9954aa89450b2d8d014c
Bill DeCoste <wdecoste@redhat.com>
2016-02-17 20:25:08 -0500

    sso tests
    update sso tests
    
    switch HttpURLConnection for HttpClient
    
    sso tests
    
    sso tests
    
    sso tests
    
    sso tests
    
    sso tests
    
    sso tests
    
    openshift.domain property
    
    run all tests
    
    add sso login test

---
+++ sso/src/test/java/org/jboss/test/arquillian/ce/sso/SsoAllInOneTest.java
@@ -0,0 +1,119 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.sso;
+
+import static junit.framework.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.jboss.arquillian.ce.api.ExternalDeployment;
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.OpenShiftResources;
+import org.jboss.arquillian.ce.api.Template;
+import org.jboss.arquillian.ce.api.TemplateParameter;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.test.arquillian.ce.sso.support.Client;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@ExternalDeployment
+@Template(url = "https://raw.githubusercontent.com/bdecoste/application-templates/ssoTP/sso/sso70-postgresql-eap64.json",
+		labels = "application=helloworld,component=eap",
+		parameters = {
+				@TemplateParameter(name = "HOSTNAME_HTTP", value="helloworld${openshift.domain}"),
+				@TemplateParameter(name = "HOSTNAME_HTTPS", value="secure-helloworld${openshift.domain}"),
+				@TemplateParameter(name = "SSO_HOSTNAME_HTTP", value="sso${openshift.domain}"),
+				@TemplateParameter(name = "SSO_HOSTNAME_HTTPS", value="secure-sso${openshift.domain}"),
+				@TemplateParameter(name = "SSO_URI", value="https://secure-sso${openshift.domain}/auth"),
+				@TemplateParameter(name = "APPLICATION_ROUTES", value="http://helloworld${openshift.domain};https://secure-helloworld${openshift.domain}"),
+                @TemplateParameter(name = "IMAGE_STREAM_NAMESPACE", value="${kubernetes.namespace}")})
+@OpenShiftResources({
+        @OpenShiftResource("classpath:sso-service-account.json"),
+        @OpenShiftResource("classpath:sso-app-secret.json"),
+        @OpenShiftResource("classpath:sso-demo-secret.json"),
+        @OpenShiftResource("classpath:eap-app-secret.json"),
+        @OpenShiftResource("classpath:ssoeap-image-stream.json"),
+        @OpenShiftResource("classpath:sso-image-stream.json"),
+        @OpenShiftResource("classpath:postgresql-image-stream.json")
+}) 
+public class SsoAllInOneTest extends SsoEapBaseTest 
+{
+	
+	public SsoAllInOneTest() {
+		route = "helloworld";
+		secureRoute = "secure-helloworld";
+	}
+	
+	@Test
+    @RunAsClient
+    public void testLogin() throws Exception {
+		login(HTTP, route + System.getProperty("openshift.domain"));
+    }
+	
+	@Test
+    @RunAsClient
+    public void testSecureLogin() throws Exception {
+		login(HTTPS, secureRoute + System.getProperty("openshift.domain"));
+	}
+	
+	protected void login(String protocol, String host) throws Exception {
+        Client client = new Client(protocol + "://" + host);
+       
+        String result = client.get("app-profile-jee/profile.jsp");
+        System.out.println("!!!!!! result " + result);
+        assertTrue(result.contains("kc-form-login"));
+    }
+	
+	@Test
+    @RunAsClient
+    public void testOidcLogin() throws Exception {
+		oidcLogin(HTTP, route + System.getProperty("openshift.domain"), "redirect_uri=http%3A%2F%2Fhelloworld" + System.getProperty("openshift.domain") + "%2Fapp-profile-jee%2Fprofile.jsp");
+	}
+	
+	@Test
+    @RunAsClient
+    public void testSecureOidcLogin() throws Exception {
+		oidcLogin(HTTPS, secureRoute + System.getProperty("openshift.domain"), "redirect_uri=https%3A%2F%2Fsecure-helloworld" + System.getProperty("openshift.domain") + "%2Fapp-profile-jee%2Fprofile.jsp");
+	}
+        
+    protected void oidcLogin(String protocol, String host, String expected) throws Exception {
+        Client client = new Client(protocol + "://" + host);
+        
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("username", "demouser"));
+        params.add(new BasicNameValuePair("password", "demopass"));
+        params.add(new BasicNameValuePair("login", "submit"));
+        
+        String result = client.post("app-profile-jee/profile.jsp",params);
+        System.out.println("!!!!!! result " + result);
+        assertTrue(result.contains(expected));
+    }
+
+}
