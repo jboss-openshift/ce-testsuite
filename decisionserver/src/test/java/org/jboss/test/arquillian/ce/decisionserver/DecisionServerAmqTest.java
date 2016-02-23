30f54fe005e16844d5f6b6f01be5afdf1d0ead8d
Ales Justin <ales.justin@gmail.com>
2016-02-23 16:40:24 +0100

    Fix label.

--- decisionserver/src/test/java/org/jboss/test/arquillian/ce/decisionserver/DecisionServerAmqTest.java
+++ decisionserver/src/test/java/org/jboss/test/arquillian/ce/decisionserver/DecisionServerAmqTest.java
@@ -76,7 +76,7 @@ import org.openshift.quickstarts.decisionserver.hellorules.Person;
 @ExternalDeployment
 //@Template(url = "https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver62-basic-s2i.json",
 @Template(url = "https://raw.githubusercontent.com/spolti/application-templates/CLOUD-435/decisionserver/decisionserver62-amq-s2i.json",
-        labels = "application=kie-app",
+        labels = "deploymentConfig=kie-app",
         parameters = {
                 @TemplateParameter(name = "KIE_SERVER_USER", value = "${kie.username:kieserver}"),
                 @TemplateParameter(name = "KIE_SERVER_PASSWORD", value = "${kie.password:Redhat@123}"),
