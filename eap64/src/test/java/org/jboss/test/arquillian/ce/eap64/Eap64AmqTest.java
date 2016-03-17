4b4cba0b9fbf5d41d88fa0a0d0abeaa6ca3cb11b
Jonh Wendell <jonh.wendell@redhat.com>
2016-03-17 14:27:12 -0300

    Add test for EAP 6.4 AMQ template

---
+++ eap64/src/test/java/org/jboss/test/arquillian/ce/eap64/Eap64AmqTest.java
@@ -0,0 +1,84 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.eap64;
+
+import java.net.URL;
+
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.Template;
+import org.jboss.arquillian.ce.api.TemplateParameter;
+import org.jboss.arquillian.ce.cube.RouteURL;
+import org.jboss.arquillian.ce.httpclient.HttpClientBuilder;
+import org.jboss.arquillian.ce.httpclient.HttpRequest;
+import org.jboss.arquillian.ce.httpclient.HttpResponse;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jonh Wendell
+ */
+
+@RunWith(Arquillian.class)
+@Template(url = "https://raw.githubusercontent.com/jboss-openshift/application-templates/master/eap/eap64-amq-s2i.json", parameters = {
+        @TemplateParameter(name = "HTTPS_NAME", value = "jboss"),
+        @TemplateParameter(name = "HTTPS_PASSWORD", value = "mykeystorepass") })
+@OpenShiftResource("classpath:eap-app-secret.json")
+public class Eap64AmqTest {
+    @RouteURL("eap-app")
+    private URL url;
+
+    @RouteURL("secure-eap-app")
+    private URL secureUrl;
+
+    @Test
+    @RunAsClient
+    public void testPageRendering() throws Exception {
+        actualTestPageRendering(url.toString());
+    }
+
+    @Test
+    @RunAsClient
+    public void testSecurePageRendering() throws Exception {
+        actualTestPageRendering(secureUrl.toString());
+    }
+
+    private void actualTestPageRendering(String baseUrl) throws Exception {
+        testPageContains(baseUrl, "URL=HelloWorldMDBServletClient");
+        testPageContains(baseUrl + "/HelloWorldMDBServletClient", "<em>queue://queue/HELLOWORLDMDBQueue</em>");
+        testPageContains(baseUrl + "/HelloWorldMDBServletClient?topic", "<em>topic://topic/HELLOWORLDMDBTopic</em>");
+    }
+
+    private void testPageContains(String url, String text) throws Exception {
+        HttpRequest request = HttpClientBuilder.doGET(url);
+        HttpResponse response = HttpClientBuilder.untrustedConnectionClient().execute(request);
+
+        Assert.assertEquals(200, response.getResponseCode());
+
+        String responseString = response.getResponseBodyAsString();
+        Assert.assertTrue(responseString.contains(text));
+    }
+}
