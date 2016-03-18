558026a813613c7feec434a2aa1ab3d923e3551f
Jonh Wendell <jonh.wendell@redhat.com>
2016-03-18 09:55:25 -0300

    Add EAP 6.4 persistent tests for postgresql and mongodb

---
+++ eap64/src/test/java/org/jboss/test/arquillian/ce/eap64/Eap64PostgresqlPersistentTest.java
@@ -0,0 +1,24 @@
+package org.jboss.test.arquillian.ce.eap64;
+
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.Template;
+import org.jboss.arquillian.ce.api.TemplateParameter;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jonh Wendell
+ */
+
+@RunWith(Arquillian.class)
+@Template(url = "https://raw.githubusercontent.com/jboss-openshift/application-templates/master/eap/eap64-postgresql-persistent-s2i.json", parameters = {
+        @TemplateParameter(name = "HTTPS_NAME", value = "jboss"),
+        @TemplateParameter(name = "HTTPS_PASSWORD", value = "mykeystorepass") })
+@OpenShiftResource("classpath:eap-app-secret.json")
+public class Eap64PostgresqlPersistentTest extends Eap64PersistentTestBase {
+
+    @Override
+    String[] getRCNames() {
+        return new String[] {"eap-app-postgresql", "eap-app"};
+    }
+}
