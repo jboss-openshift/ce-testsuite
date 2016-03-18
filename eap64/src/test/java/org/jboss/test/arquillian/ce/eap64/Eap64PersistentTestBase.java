cdad9e74ff3112b221b0f05a05a97f15ac070e34
Jonh Wendell <jonh.wendell@redhat.com>
2016-03-18 09:33:10 -0300

    Refactor EAP 6.4 Persistent tests to have an abstract class

---
+++ eap64/src/test/java/org/jboss/test/arquillian/ce/eap64/Eap64PersistentTestBase.java
@@ -0,0 +1,66 @@
+package org.jboss.test.arquillian.ce.eap64;
+
+import java.net.URL;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.ce.api.OpenShiftHandle;
+import org.jboss.arquillian.ce.common.TodoList;
+import org.jboss.arquillian.ce.cube.RouteURL;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Test;
+
+/**
+ * @author Jonh Wendell
+ */
+
+public abstract class Eap64PersistentTestBase {
+    private final static Logger log = Logger.getLogger(Eap64PersistentTestBase.class.getName());
+
+    abstract String[] getRCNames();
+
+    @RouteURL("eap-app")
+    private URL url;
+
+    @RouteURL("secure-eap-app")
+    private URL secureUrl;
+
+    private final static String summary1 = UUID.randomUUID().toString();
+    private final static String summary2 = UUID.randomUUID().toString();
+    private final static String description1 = UUID.randomUUID().toString();
+    private final static String description2 = UUID.randomUUID().toString();
+
+    @Test
+    @RunAsClient
+    @InSequence(1)
+    public void InsertItems() throws Exception {
+        TodoList.insertItem(url.toString(), summary1, description1);
+        TodoList.insertItem(secureUrl.toString(), summary2, description2);
+    }
+
+    private void restartPod(OpenShiftHandle adapter, String name) throws Exception {
+        log.info("Scaling down " + name);
+        adapter.scaleDeployment(name, 0);
+        log.info("Scaling up " + name);
+        adapter.scaleDeployment(name, 1);
+    }
+
+    @Test
+    @RunAsClient
+    @InSequence(2)
+    public void restartPods(@ArquillianResource OpenShiftHandle adapter) throws Exception {
+        for (String rc : getRCNames()) {
+            restartPod(adapter, rc);
+        }
+    }
+
+    @Test
+    @RunAsClient
+    @InSequence(3)
+    public void verifyItems() throws Exception {
+        TodoList.checkItem(url.toString(), summary1, description1);
+        TodoList.checkItem(secureUrl.toString(), summary2, description2);
+    }
+}
