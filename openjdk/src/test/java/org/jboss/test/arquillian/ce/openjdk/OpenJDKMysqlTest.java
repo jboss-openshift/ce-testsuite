56e745e07fd3065ee875be41d802bd2ebee0352d
Douglas Palmer <dpalmer@redhat.com>
2017-03-17 12:01:03 -0700

    Added openjdk tests

---
+++ openjdk/src/test/java/org/jboss/test/arquillian/ce/openjdk/OpenJDKMysqlTest.java
@@ -0,0 +1,21 @@
+package org.jboss.test.arquillian.ce.openjdk;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.OpenShiftResources;
+import org.jboss.arquillian.ce.api.Template;
+import org.jboss.arquillian.ce.api.TemplateParameter;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@Template(url = "file://${user.dir}/src/test/resources/openjdk18-mysql-s2i.json")
+@OpenShiftResources({
+    @OpenShiftResource("https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/secrets/openjdk-app-secret.json")
+})
+
+public class OpenJDKMysqlTest extends OpenJDKDatabaseTest {
+
+    public OpenJDKMysqlTest() {
+        super(false, "mysql");
+    }
+}
