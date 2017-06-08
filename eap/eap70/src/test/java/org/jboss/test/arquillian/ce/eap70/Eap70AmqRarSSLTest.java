85a324054240f059c7d710a86a090309510431f6
Filippe Spolti <fspolti@redhat.com>
2017-06-08 16:22:38 -0300

    Add tests to cover the changes introduced on CLOUD-106

---
+++ eap/eap70/src/test/java/org/jboss/test/arquillian/ce/eap70/Eap70AmqRarSSLTest.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.eap70;
+
+import org.jboss.arquillian.ce.api.OpenShiftResource;
+import org.jboss.arquillian.ce.api.OpenShiftResources;
+import org.jboss.arquillian.ce.api.Template;
+import org.jboss.arquillian.ce.api.TemplateParameter;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.test.arquillian.ce.eap.common.EapAmqTestBase;
+import org.junit.runner.RunWith;
+
+/**
+ * @author spolti
+ */
+@RunWith(Arquillian.class)
+@Template(url = "https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/eap/eap70-amq-ssl-s2i.json", parameters = {
+        @TemplateParameter(name = "HTTPS_NAME", value = "jboss"),
+        @TemplateParameter(name = "HTTPS_PASSWORD", value = "mykeystorepass"),
+        @TemplateParameter(name = "MQ_CLIENT_KEYSTORE_PASSWORD", value = "password"),
+        @TemplateParameter(name = "AMQ_TRUSTSTORE_PASSWORD", value = "password"),
+        @TemplateParameter(name = "AMQ_KEYSTORE_PASSWORD", value = "password"),
+        @TemplateParameter(name = "IMAGE_STREAM_NAMESPACE", value = "${image.stream.namespace:openshift}")
+})
+@OpenShiftResources({
+        @OpenShiftResource("https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/secrets/eap7-app-secret.json"),
+        @OpenShiftResource("https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/secrets/amq-app-secret.json")
+})
+public class Eap70AmqRarSSLTest extends EapAmqTestBase {}
