c094c24631c3ce252d7ab8de01875f5e337c56a9
Filippe Spolti <fspolti@redhat.com>
2017-08-01 12:52:36 -0300

    Remove unused imports

--- datavirt/src/test/java/org/jboss/test/arquillian/ce/datavirt/DatavirtTestBase.java
+++ datavirt/src/test/java/org/jboss/test/arquillian/ce/datavirt/DatavirtTestBase.java
@@ -23,36 +23,21 @@
 
 package org.jboss.test.arquillian.ce.datavirt;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.util.Collections;
-import java.util.Map;
-
-import org.jboss.arquillian.ce.api.OpenShiftHandle;
-import org.jboss.arquillian.ce.httpclient.HttpClient;
-import org.jboss.arquillian.ce.httpclient.HttpClientBuilder;
 import org.jboss.arquillian.ce.httpclient.HttpClientExecuteOptions;
-import org.jboss.arquillian.ce.httpclient.HttpRequest;
-import org.jboss.arquillian.ce.httpclient.HttpResponse;
 import org.jboss.arquillian.ce.shrinkwrap.Libraries;
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.test.arquillian.ce.datavirt.support.JDBCClient;
-import org.junit.Test;
+
+import java.io.IOException;
 
 public abstract class DatavirtTestBase {
-	
-	private final HttpClientExecuteOptions execOptions = new HttpClientExecuteOptions.Builder().tries(3)
+
+    private final HttpClientExecuteOptions execOptions = new HttpClientExecuteOptions.Builder().tries(3)
             .desiredStatusCode(200).delay(10).build();
-	
-	protected static WebArchive getDeploymentBase(Class... classes) throws IOException {
+
+    protected static WebArchive getDeploymentBase(Class... classes) throws IOException {
         WebArchive war = ShrinkWrap.create(WebArchive.class, "run-in-pod.war");
         war.setWebXML(new StringAsset("<web-app/>"));
         war.addClass(DatavirtTestBase.class);
@@ -61,16 +46,15 @@ public abstract class DatavirtTestBase {
 
         war.addAsLibraries(Libraries.transitive("org.jboss.teiid", "teiid-jdbc"));
         war.addAsLibraries(Libraries.transitive("org.jboss.arquillian.container", "arquillian-ce-httpclient"));
-        
+
         war.addAsResource("keystore.jks");
-        
+
         return war;
     }
-	
-	protected JDBCClient createJdbcClient() throws Exception {
+
+    protected JDBCClient createJdbcClient() throws Exception {
         return new JDBCClient();
     }
-	
-	
+
 
 }
